e4b60ee2aa09f8c6368cb746250b2e658bdc90f7
Swann Williame <swann.williame@gmail.com>
2024-03-14 10:36:18 +0100

    changer la couleur de fond de l'app

--- core/src/fr/studiokakou/kakouquest/Main.java
+++ core/src/fr/studiokakou/kakouquest/Main.java
@@ -1,31 +1,30 @@
 package fr.studiokakou.kakouquest;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		Gdx.gl.glClearColor(34/255f, 34/255f, 34/255f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
