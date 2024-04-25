fcc603de7aa723a6e9ab5c3a911dd2da551e433b
Fabrice GERBAUD <fabricegerbaud06@gmail.com>
2024-04-25 09:43:37 +0200

    Update les Settings
    J'ai rajouté toutes les textes des Settings en PNG.
    Je les ai placés sur le screen, la width de certains et à revoir.

--- core/src/fr/studiokakou/kakouquest/screens/SettingsScreen.java
+++ core/src/fr/studiokakou/kakouquest/screens/SettingsScreen.java
@@ -19,12 +19,46 @@ import fr.studiokakou.kakouquest.weapon.MeleeWeapon;
 import static com.badlogic.gdx.Gdx.input;
 
 public class SettingsScreen implements Screen{
+    /**
+     * Constants for the text textures
+     */
+    private final static int TEXT_WIDTH = Gdx.graphics.getWidth()/4;
+    private final static int TEXT_HEIGHT = TEXT_WIDTH/6;
+    /**
+     * Coords for the text textures
+     */
+    int ecart = 50;
+    int xposText = Gdx.graphics.getWidth()/4 * 1;
+    int yposUp = Gdx.graphics.getHeight() - 150;
+    int yposDown = yposUp- TEXT_HEIGHT - ecart;
+    int yposLeft = yposDown - TEXT_HEIGHT - ecart;
+    int yposRight = yposLeft - TEXT_HEIGHT - ecart;
+    int yposInteract = yposRight - TEXT_HEIGHT - ecart;
+    int yposDash = yposInteract - TEXT_HEIGHT - ecart;
+    int yposInventory = yposDash - TEXT_HEIGHT - ecart;
     GameSpace game;
+
+    Texture upText;
+    Texture downText;
+    Texture leftText;
+    Texture rightText;
+    Texture interactText;
+    Texture dashText;
+    Texture inventoryText;
     Texture backButton;
 
     public SettingsScreen(GameSpace game){
         this.game = game;
+        upText = new Texture("assets/window/up_text.png");
+        downText = new Texture("assets/window/down_text.png");
+        leftText = new Texture("assets/window/left_text.png");
+        rightText = new Texture("assets/window/right_text.png");
+        interactText = new Texture("assets/window/interact_text.png");
+        dashText = new Texture("assets/window/dash_text.png");
+        inventoryText = new Texture("assets/window/inventory_text.png");
         backButton = new Texture("assets/buttons/back_button.png");
+
+
     }
     @Override
     public void show() {
@@ -46,6 +80,7 @@ public class SettingsScreen implements Screen{
         Gdx.gl.glClearColor(34/255f, 34/255f, 34/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
+
         game.batch.draw(backButton, 30, 30, 230, 70);
         if (Gdx.input.getX() < 30 + 230 &&
                 Gdx.input.getX() > 30 &&
@@ -56,6 +91,16 @@ public class SettingsScreen implements Screen{
                 game.setScreen(new MenuScreen(game));
             }
         }
+
+        game.batch.draw(upText, xposText, yposUp, TEXT_WIDTH, TEXT_HEIGHT);
+        game.batch.draw(downText, xposText, yposDown, TEXT_WIDTH, TEXT_HEIGHT);
+        game.batch.draw(leftText, xposText, yposLeft, TEXT_WIDTH, TEXT_HEIGHT);
+        game.batch.draw(rightText, xposText, yposRight, TEXT_WIDTH, TEXT_HEIGHT);
+        game.batch.draw(interactText, xposText, yposInteract, TEXT_WIDTH, TEXT_HEIGHT);
+        game.batch.draw(dashText, xposText, yposDash, TEXT_WIDTH, TEXT_HEIGHT);
+        game.batch.draw(inventoryText, xposText, yposInventory, TEXT_WIDTH, TEXT_HEIGHT);
+
+
         game.batch.end();
     }
 
