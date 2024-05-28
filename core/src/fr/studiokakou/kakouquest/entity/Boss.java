3518470f0d257d9417ec5c77b13dcbb37db234fb
Fabrice GERBAUD <fabricegerbaud06@gmail.com>
2024-05-28 12:52:09 +0200

    Monstres bientôt fonctionnels
    - Rajout du fonctionnement des monstres
    - Choses qu'il manque encore : (better death animation, boss moves towards player, Mettre le boss tous les 5 niveaux)

--- core/src/fr/studiokakou/kakouquest/entity/Boss.java
+++ core/src/fr/studiokakou/kakouquest/entity/Boss.java
@@ -5,10 +5,10 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import fr.studiokakou.kakouquest.map.Map;
 import fr.studiokakou.kakouquest.map.Point;
 import fr.studiokakou.kakouquest.player.Player;
-import fr.studiokakou.kakouquest.screens.InGameScreen;
 import fr.studiokakou.kakouquest.utils.Utils;
 
 import java.time.LocalDateTime;
@@ -20,6 +20,14 @@ public class Boss extends Monster {
     boolean isAttacking = false;
     boolean isHit = false;
 
+    private float stateTime;
+    private float hitStateTime;
+    private float attackStateTime;
+    private float deathStateTime;
+    private final float HIT_ANIMATION_DURATION = 0.3f; // durée de l'animation hit en secondes
+    private final float ATTACK_ANIMATION_DURATION = 1.0f; // durée de l'animation attack en secondes
+    private final float DEATH_ANIMATION_DURATION = 5.0f; // durée de l'animation death en secondes
+
     public Boss(String name, String idleAnimationPath, String runAnimationPath, String attackAnimationPath,
                 String hitAnimationPath, String deathAnimationPath, int hp, int damage, float attackPause, float speed,
                 int detectRange, int currentLevel) {
@@ -27,8 +35,16 @@ public class Boss extends Monster {
         this.idleAnimation = Utils.getAnimation(idleAnimationPath, 6, 1);
         this.runAnimation = Utils.getAnimation(runAnimationPath, 12, 1);
         this.attackAnimation = Utils.getAnimation(attackAnimationPath, 15, 1);
-        this.hitAnimation = Utils.getAnimation(hitAnimationPath, 5, 1);
-        this.deathAnimation = Utils.getAnimation(deathAnimationPath, 22, 1);
+
+        // Ajuster la durée de chaque frame pour l'animation hit
+        int hitFrameCount = 5;
+        this.hitAnimation = new Animation<>(HIT_ANIMATION_DURATION / hitFrameCount, Utils.getAnimation(hitAnimationPath, hitFrameCount, 1).getKeyFrames());
+
+        // Ajuster la durée de chaque frame pour l'animation death
+        int deathFrameCount = 22;
+        this.deathAnimation = new Animation<>(DEATH_ANIMATION_DURATION / deathFrameCount, Utils.getAnimation(deathAnimationPath, deathFrameCount, 1).getKeyFrames());
+
+        this.sprite = new Sprite(idleAnimation.getKeyFrame(0)); // Initialiser le sprite avec la première frame de l'animation idle
     }
 
     @Override
@@ -37,75 +53,85 @@ public class Boss extends Monster {
             return;
         }
         Point playerPos = player.pos;
+        if (Utils.distance(playerPos, this.pos) <= 20) {
+            this.attack(player);
+        }
         if (detectPlayer(playerPos)) {
             this.isRunning = true;
             this.getOrientation(player);
             Point orientation = Point.getOrientation(this.pos, playerPos);
             if (canMove(new Point(orientation.x, 0), map)) {
-                this.pos = this.pos.add(orientation.x * (this.speed) * Gdx.graphics.getDeltaTime(), 0);
+                this.pos = this.pos.add(orientation.x * this.speed * Gdx.graphics.getDeltaTime(), 0);
             }
             if (canMove(new Point(0, orientation.y), map)) {
-                this.pos = this.pos.add(0, orientation.y * (this.speed) * Gdx.graphics.getDeltaTime());
+                this.pos = this.pos.add(0, orientation.y * this.speed * Gdx.graphics.getDeltaTime());
             }
+
+            // Tourner le boss vers le joueur
+            this.isFlip = (this.pos.x < player.pos.x);
         } else {
             this.isRunning = false;
         }
     }
 
+
     @Override
     public void draw(SpriteBatch batch) {
-        TextureRegion currentFrame;
+        stateTime += Gdx.graphics.getDeltaTime();
+        TextureRegion currentFrame = null;
 
         if (isDying) {
-            currentFrame = this.deathAnimation.getKeyFrame(InGameScreen.stateTime, false);
-            if (this.deathAnimation.isAnimationFinished(InGameScreen.stateTime)) {
+            deathStateTime += Gdx.graphics.getDeltaTime();
+            currentFrame = this.deathAnimation.getKeyFrame(deathStateTime, false);
+            if (this.deathAnimation.isAnimationFinished(deathStateTime)) {
                 this.isDead = true;
             }
         } else if (isHit) {
-            currentFrame = this.hitAnimation.getKeyFrame(InGameScreen.stateTime, false);
-            if (this.hitAnimation.isAnimationFinished(InGameScreen.stateTime)) {
+            hitStateTime += Gdx.graphics.getDeltaTime();
+            currentFrame = this.hitAnimation.getKeyFrame(hitStateTime, false);
+            if (this.hitAnimation.isAnimationFinished(hitStateTime)) {
                 this.isHit = false;
+                this.isRed = false;
             }
         } else if (isAttacking) {
-            currentFrame = this.attackAnimation.getKeyFrame(InGameScreen.stateTime, false);
-            if (this.attackAnimation.isAnimationFinished(InGameScreen.stateTime)) {
+            attackStateTime += Gdx.graphics.getDeltaTime();
+            currentFrame = this.attackAnimation.getKeyFrame(attackStateTime, false);
+            if (this.attackAnimation.isAnimationFinished(attackStateTime)) {
                 this.isAttacking = false;
             }
         } else if (isRunning) {
-            currentFrame = this.runAnimation.getKeyFrame(InGameScreen.stateTime, true);
+            currentFrame = this.runAnimation.getKeyFrame(stateTime, true);
         } else {
-            currentFrame = this.idleAnimation.getKeyFrame(InGameScreen.stateTime, true);
+            currentFrame = this.idleAnimation.getKeyFrame(stateTime, true);
         }
 
-        this.sprite = new Sprite(currentFrame);
+        if (currentFrame != null) {
+            this.sprite.setRegion(currentFrame);
 
-        this.sprite.setX(this.pos.x);
-        this.sprite.setY(this.pos.y);
+            // Centrer le sprite sur les coordonnées du boss
+            this.sprite.setX(this.pos.x - this.sprite.getWidth() / 2);
+            this.sprite.setY(this.pos.y - this.sprite.getHeight() / 2);
 
-        this.sprite.flip(this.isFlip, false);
+            this.sprite.setFlip(this.isFlip, false); // Utiliser setFlip pour mettre à jour l'orientation
 
-        // Only set red color for a short period to indicate taking damage
-        if (isRed) {
-            currentFrame = this.hitAnimation.getKeyFrame(InGameScreen.stateTime, false);
-            if (this.hitAnimation.isAnimationFinished(InGameScreen.stateTime)) {
-                this.isHit = false;
-            }
-        } else {
-            currentFrame = this.idleAnimation.getKeyFrame(InGameScreen.stateTime, true);
+            this.sprite.draw(batch);
         }
-
-        this.sprite.draw(batch);
     }
 
     @Override
-    public void takeDamage(Player player){
-        this.hp -= player.currentWeapon.damage*(player.strength/10);
-        this.isRed = true;
-        if (this.hp <= 0){
-            this.isAttacking=false;
-            this.isHit=false;
-            this.isRunning=false;
-            this.isDying=true;
+    public void takeDamage(Player player) {
+        if (Utils.getDistance(player.pos, this.pos) < 80 ){
+            this.hp -= player.currentWeapon.damage * (player.strength / 10);
+            this.isHit = true;
+            this.isRed = true; // Activer l'effet rouge
+            hitStateTime = 0; // Réinitialiser hitStateTime lors de la prise de dégâts
+            if (this.hp <= 0) {
+                this.isAttacking = false;
+                this.isHit = false;
+                this.isRunning = false;
+                this.isDying = true;
+                deathStateTime = 0; // Réinitialiser deathStateTime lors de la mort
+            }
         }
     }
 
@@ -117,8 +143,14 @@ public class Boss extends Monster {
             player.takeDamage(this.damage);
             this.currentAttackTime = LocalDateTime.now();
             this.isAttacking = true;
+            attackStateTime = 0; // Réinitialiser attackStateTime lors du début de l'attaque
+
+            // Calculer l'orientation du boss par rapport au joueur
+            this.isFlip = (this.pos.x + 150 < player.pos.x);
         }
     }
+
+
 
     public static Boss createSlimeBoss(int currentLevel) {
         return new Boss("Slime Boss",
@@ -127,6 +159,6 @@ public class Boss extends Monster {
                 "assets/entities/slime_boss_attack.png",
                 "assets/entities/slime_boss_hit.png",
                 "assets/entities/slime_boss_death.png",
-                2000, 50, 1000, 30f, 300, currentLevel);
+                2000, 50, 300, 30f, 150, currentLevel); // Réduire attackPause à 500 ms pour des attaques plus fréquentes
     }
 }
--- core/src/fr/studiokakou/kakouquest/entity/Monster.java
+++ core/src/fr/studiokakou/kakouquest/entity/Monster.java
@@ -324,6 +324,7 @@ public class Monster {
         possibleMonsters.put(10, new ArrayList<>());
         possibleMonsters.put(11, new ArrayList<>());
         possibleMonsters.put(12, new ArrayList<>());
+        possibleMonsters.put(13, new ArrayList<>());
 
 
         possibleMonsters.get(10).add(BIG_DEMON(currentLevel));
@@ -339,12 +340,10 @@ public class Monster {
         possibleMonsters.get(7).add(SWAMPY(currentLevel));
         possibleMonsters.get(1).add(TINY_ZOMBIE(currentLevel));
         possibleMonsters.get(4).add(WOGOL(currentLevel));
+        possibleMonsters.get(13).add(Boss.createSlimeBoss(currentLevel));
 
-        // Add Slime Boss every 5 levels
-        if (currentLevel == 1){
-            possibleMonsters.get(currentLevel).add(Boss.createSlimeBoss(currentLevel));
-        }
-        if (currentLevel % 5 == 0) {
+        // Add Slime Boss from level 8
+        if (currentLevel == 1) {
             possibleMonsters.get(currentLevel).add(Boss.createSlimeBoss(currentLevel));
         }
     }
