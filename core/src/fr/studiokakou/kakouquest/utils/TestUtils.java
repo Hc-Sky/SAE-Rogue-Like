bee5c586cdb9d76563cc63f0de32cc7ce390a8bc
Swann Williame <swann.williame@gmail.com>
2024-06-12 17:15:34 +0200

    ajout v0.1 du unit_test

---
+++ core/src/fr/studiokakou/kakouquest/utils/TestUtils.java
@@ -0,0 +1,14 @@
+package fr.studiokakou.kakouquest.utils;
+
+import fr.studiokakou.kakouquest.map.Point;
+
+public class TestUtils {
+    public static void main(String[] args) {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(4, 8);
+
+        float speed = 2.0f;
+
+        assert Utils.getPointDirection(p1, p2, speed).equals(new Point((float) 1.8944272, (float) 3.7888544)) : "Test failed";
+    }
+}
